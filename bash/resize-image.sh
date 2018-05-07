d7eb1d97328629c03ae0c8384262ed8d8e41ac85
ben <benoit.works@gmail.com>
2018-05-07 10:31:21 +0200

    enhance resize script

--- bash/resize-image.sh
+++ bash/resize-image.sh
@@ -1,19 +1,68 @@
 #!/bin/bash
-#USAGE: put this resize-image in the folder to process. 
-#chmod +x resize-image.sh 
-#./resize-image.sh
 
-SIZE_PERCENTAGE=20
-DESTINATION_FOLDER='image-resized'
-EXTENSION_FILES_TO_PROCESS='jpg'
+#USAGE: ~/personal-linux-config/bash/resize-image.sh -h
+
+DEFAULT_SIZE_PERCENTAGE=20
+SIZE_PERCENTAGE=$DEFAULT_SIZE_PERCENTAGE
+declare -a EXTENSION_TO_PROCESS=("jpg" "JPG" "png" "PNG");
+
+usage() {
+cat << EndOfMessage
+#cd Pictures
+#use DEFAULT_SIZE_PERCENTAGE:
+#~/personal-linux-config/bash/resize-image.sh
+#OR specify resize percent using -p option:
+#~/personal-linux-config/bash/resize-image.sh -p 10
+EndOfMessage
+}
+
+while getopts ":p:h" opt; do
+  case $opt in
+    h)
+      usage
+      exit 0
+      ;;
+    p)
+      echo "-p was triggered, Parameter: $OPTARG" >&2
+      SIZE_PERCENTAGE=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
 
+in_array() {
+    local haystack=${1}[@]
+    local needle=${2}
+    for i in ${!haystack}; do
+        if [[ ${i} == ${needle} ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+DESTINATION_FOLDER="image-resized-$SIZE_PERCENTAGE-percent"
 mkdir ${DESTINATION_FOLDER}
-for i in *.${EXTENSION_FILES_TO_PROCESS}
+for file in *
 do
-	FILENAME=$(basename "$i")
-	EXTENSION="${FILENAME##*.}"
-	FILENAME="${FILENAME%.*}"
-	echo "resizing "${i} : filename is ${FILENAME} and extension is ${EXTENSION}
-	convert -resize ${SIZE_PERCENTAGE}% ${i} ${DESTINATION_FOLDER}/${FILENAME}-resized-${SIZE_PERCENTAGE}"."${EXTENSION}
+    if [[ -f $file ]]; then
+        FILENAME=$(basename "$file")
+        EXTENSION="${FILENAME##*.}"
+        FILENAME="${FILENAME%.*}"
+        echo "evaluating ${file} : filename is ${FILENAME} and extension is ${EXTENSION}"
+        if in_array EXTENSION_TO_PROCESS "$EXTENSION"; then
+            echo 'converting'
+            convert -resize ${SIZE_PERCENTAGE}% ${file} ${DESTINATION_FOLDER}/${FILENAME}-resized-${SIZE_PERCENTAGE}"."${EXTENSION}
+        else
+            echo 'skipping'
+        fi
+	fi
  done
