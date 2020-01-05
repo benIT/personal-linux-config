5948330c8eaf45c9fa60eaa4e3796c4c81a56d95
benIT <benoit.works@gmail.com>
2020-01-05 16:05:41 +0100

    basic reencoding script

---
+++ bash/reencode-camcorder-video.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for VIDEO in *.MTS
+do
+	FILENAME=$(basename "$VIDEO")
+	EXTENSION="${FILENAME##*.}"
+	FILENAME="${FILENAME%.*}"
+    MODIF_DATE=$(date -r ${VIDEO} +%Y-%m-%d-%H-%M-%S)
+   	echo "encoding to mp4 ${VIDEO} : filename is ${FILENAME} | extension is ${EXTENSION} | modif date : $MODIF_DATE"
+   	ffmpeg -i ${VIDEO} ${FILENAME}.mp4
+    mkdir -p mp4
+    mv *.mp4 mp4
+done
