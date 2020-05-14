6fb0bb15220dde5014ede11f45536551cd9612a1
benIT <benoit.works@gmail.com>
2020-05-14 16:05:59 +0200

    ip script

--- bash/ip-http-proxy.sh
+++ bash/ip-http-proxy.sh
@@ -5,23 +5,18 @@
 # chmod +x /usr/local/bin/ip-http-proxy.sh
 # touch /var/log/ip-http-proxy.log
 # ip-http-proxy.sh >> /var/log/ip-http-proxy.log
-# * * * * * /usr/local/bin/ip-http-proxy.sh >> /var/log/ip-http-proxy.sh.log 2>&1
+# At every 30th minute past every hour from 8 through 18 on every day-of-week from Monday through Friday.
+# */30 8-18 * * 1-5 /usr/local/bin/ip-http-proxy.sh >> /var/log/ip-http-proxy.sh.log 2>&1
 # analyse log:
 # grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'  /var/log/ip-http-proxy.log | uniq
 
 function log {
     echo "[$(date --rfc-3339=seconds)] : $1 : $2"
 }
-current_hour=$(date +%H)
-if [  $current_hour -ge 8 ] && [ $current_hour -le 18 ] ; then
-    log "INFO" "running"
-    if [ -z "$http_proxy" ] || [ -z "$https_proxy" ]
-    then
-          log "ERROR" "\$http_proxy or \$https_proxy is missing."
-          exit 1
-    fi
-    IP=$(curl --request GET -sL --url 'https://ifconfig.me')
-    log "INFO" "$IP"
-else
-     log "INFO" "skipping"
+if [ -z "$http_proxy" ] || [ -z "$https_proxy" ]
+then
+  log "ERROR" "\$http_proxy or \$https_proxy is missing."
+  exit 1
 fi
+IP=$(curl --request GET -sL --url 'https://ifconfig.me')
+log "INFO" "$IP"
