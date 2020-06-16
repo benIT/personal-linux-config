45fd3db397c79cd82799ecc0eccd164bb612ddec
benIT <benoit.works@gmail.com>
2020-06-16 10:58:58 +0200

    temp dem script

---
+++ bash/download-dem.sh
@@ -0,0 +1,22 @@
+#script pour télécharger manuellement les dem (données altimétriques pour viking)
+#https://sourceforge.net/p/viking/discussion/general/thread/8abe1601/#2c3d
+mkdir -p ~/dem
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N44E001.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N44E002.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N44E003.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N44E004.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N44E005.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N44E006.hgt.zip  -P ~/dem &
+
+
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N45E001.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N45E002.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N45E003.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N45E004.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N45E005.hgt.zip  -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N44E007.hgt.zip  -P ~/dem &
+
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N46E001.hgt.zip -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N46E002.hgt.zip -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N46E003.hgt.zip -P ~/dem &
+wget dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N46E004.hgt.zip -P ~/dem &
